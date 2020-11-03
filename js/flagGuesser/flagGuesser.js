9d1cf385267abd347b4a143a7b8bd1e63cf0d568
remi <remi.longin@outlook.fr>
2020-11-03 14:38:04 +0100

    Valider les reponses #5 et Afficher le resultat #2

--- js/flagGuesser/flagGuesser.js
+++ js/flagGuesser/flagGuesser.js
@@ -1,7 +1,8 @@
 // Variables globales
 
 let state = {
-    question: null
+    question: null,
+    answer: null
 }
 
 let countries = [];
@@ -11,10 +12,18 @@ let questionTotal = 1;
 
 const init = async () => {
     state.question = document.querySelector("#question");
+    state.answer = document.querySelector("#answer");
 
     // Acces à toutes les informations des pays
     const response = await fetch("https://restcountries.eu/rest/v2/all");
     countries = await response.json();
+
+    state.question.querySelector('ul').addEventListener('click',({target}) => {
+        if (target.matches('li')){
+             const userAnswer = target.innerHTML;
+             checkAnswer(userAnswer);
+        }
+    });
 
     pickQuestion();
 }
@@ -60,10 +69,40 @@ const createQuestion = (countries) => {
         return a.charCodeAt(0) - b.charCodeAt(0);
     });
 
-    const question = {
+    const questions = {
         flag: country.flag,  //on recupere le drapeau
+        answer: country.name,
         possibilities
     }
 
-    return question;
+    return questions;
+}
+
+const switchPanel = (states) =>{
+    switch(states){
+        case 'answer':
+            state.answer.style.display = 'block';
+            state.question.style.display = 'none';
+            break;
+        case 'question':
+            state.answer.style.display = 'none';
+            state.question.style.display = 'block';
+            break;
+        default:
+            state.answer.style.display = 'none';
+            state.question.style.display = 'none';
+            break;
+    }
+};
+
+const checkAnswer = (userAnswer) => {
+    if(userAnswer === questions.answer){
+        state.answer.querySelector('h2').innerHTML ='Good answer';
+        state.answer.querySelector('p').innerHTML ='';
+    }else{
+        state.answer.querySelector('h2').innerHTML ='Wrong answer';
+        state.answer.querySelector('p').innerHTML =`The answer was: ${questions.answer}`;
+
+    }
+    switchPanel('answer');
 }
