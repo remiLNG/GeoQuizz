38f6b608c600cbe9b3a2c6a4f597b3b5ee7348a3
remi <remi.longin@outlook.fr>
2020-11-15 12:42:05 +0100

    FlagFinder : Valider les reponses #25 et Afficher le resultat #17

--- GeoQuizz/js/flagFinder/flagFinder.js
+++ GeoQuizz/js/flagFinder/flagFinder.js
@@ -2,6 +2,7 @@
 
 let state = {
     question: null,
+    answer: null,
 
 }
 
@@ -13,11 +14,13 @@ let questionTotal = 1;
 
 const init = async () => {
     state.question = document.querySelector("#question");
+    state.answer = document.querySelector("#answer");
 
     // Acces à toutes les informations des pays
     const response = await fetch("https://restcountries.eu/rest/v2/all");
     countries = await response.json();
     genererateQuestion();
+    getAnswer();
 
 }
 
@@ -42,6 +45,7 @@ const genererateQuestion = () => {
 
 window.onload = init;
 
+
 const createQuestion = (countries) => {
     const random = parseInt(Math.random() * countries.length);
 
@@ -50,14 +54,14 @@ const createQuestion = (countries) => {
 
     var possibilities = [];
 
-   //On affiche 3 drapeaux aléatoires
+    //On affiche 3 drapeaux aléatoires
     while (possibilities.length < 3) {
         var r;
         var flag1;
         var flag2;
         var flag3;
         if (flag1 == null) {
-            r= parseInt(Math.random() * countries.length)
+            r = parseInt(Math.random() * countries.length)
             flag1 = countries[r].flag;
             possibilities.push(flag1);
         } else if (flag2 == null) {
@@ -87,12 +91,84 @@ const createQuestion = (countries) => {
     }
     possibilities = randomize(possibilities)
 
-    
+
     // Tout ce qui va nous servir à creer la question (un drapeau, un choix de reponse, une bonne reponse)
     const questions = {
         pays: country.translations.fr,
-        possibilities
+        possibilities,
+        answer: country.flag
     }
 
     return questions;
 }
+
+
+
+//Passer d'un état à l'autre (question, verification de reponse)
+const switchState = (states) => {
+    switch (states) {
+        case 'answer':
+            state.answer.style.display = 'block';
+            state.question.style.display = 'none';
+            break;
+        case 'question':
+            state.answer.style.display = 'none';
+            state.question.style.display = 'block';
+
+            break;
+        default:
+            state.answer.style.display = 'none';
+            state.question.style.display = 'none';
+            break;
+    }
+};
+
+
+const getAnswer = () => {
+    var userAnswer = questions.possibilities[0];
+    state.question.querySelector('#flag').addEventListener('click', function () {
+        if (userAnswer == questions.possibilities[2]) {
+            checkAnswer(userAnswer)
+        } else
+            checkAnswer(userAnswer)
+    })
+    state.question.querySelector('#flag2').addEventListener('click', function () {
+        userAnswer = questions.possibilities[1]
+        if (userAnswer == questions.possibilities[1]) {
+            checkAnswer(userAnswer)
+        } else
+            checkAnswer(userAnswer)
+    })
+    state.question.querySelector('#flag3').addEventListener('click', function () {
+        userAnswer = questions.possibilities[2]
+        if (userAnswer == questions.possibilities[2]) {
+            checkAnswer(userAnswer)
+        } else
+            checkAnswer(userAnswer)
+    })
+    state.question.querySelector('#flag4').addEventListener('click', function () {
+        userAnswer = questions.possibilities[3]
+        if (userAnswer == questions.possibilities[3]) {
+            checkAnswer(userAnswer)
+        } else
+            checkAnswer(userAnswer)
+    })
+
+}
+
+// Verifier si c'est la bonne reponse
+const checkAnswer = (userAnswer) => {
+    // si oui alors bonne reponse
+    if (userAnswer === questions.answer) {
+        state.answer.querySelector('h2').innerHTML = 'Bonne réponse';
+    } else {
+        // si non alors mauvais reponse
+        state.answer.querySelector('h2').innerHTML = 'Mauvaise réponse';
+        state.answer.querySelector('p').innerHTML = 'La réponse était  : ';
+        state.answer.querySelector('#goodflag').setAttribute("src", questions.answer);
+    }
+    //afficher la reponse dans state answer
+    switchState('answer');
+}
+
+
