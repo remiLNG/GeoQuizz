3c19aec0191f09932f4e9fb9161b0409e0659495
Corentin <corentingarnier06@gmail.com>
2020-11-18 13:54:09 +0100

    installation et initialisation des tests unitaires #21

---
+++ GeoQuizz/test/flagFinderSpec.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var FlagFinder = require('../js/flagFinder/flagFinder')
+
+describe('Test de flagFinder',function(){
+
+    describe('genererateQuestion',function(){
+
+        it('should generate a question',function(){
+
+        })
+
+        it('should create a question',function(){
+
+        })
+
+        it('should change the state of the game',function() {
+
+        })
+
+        it('should get the answer of the player',function() {
+
+        })
+
+        it('should check the answer of the player',function(){
+
+        })
+        
+    })
+})
