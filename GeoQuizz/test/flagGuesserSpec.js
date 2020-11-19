5c255771feb25388e2681bc7d2f8660db531b3ca
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2020-11-19 16:37:25 +0100

    creation du test flagGuesserSpec.js

---
+++ GeoQuizz/test/flagGuesserSpec.js
@@ -0,0 +1,21 @@
+const { strictEqual } = require('assert');
+//var assert = require('assert');
+const chai = require('chai')
+    , assert = chai.assert
+    , expect = chai.expect
+    , should = chai.should()
+
+
+var FlagFinder = require('../js/flagGuesser/flagGuesser')
+
+describe('Test de flagGuesser', function () {
+
+    describe('createQuestion', function () {
+
+        it('should return a question', function () {
+
+            expect(createQuestion).to.be.a('object')
+
+        })
+    })
+})
