a45f49f0f7a5b51b4f42b3649f01968cfb0f0afb
Bolier <bolier.raphael@gmail.com>
2020-11-20 16:02:31 +0100

    #19 #20 Design des différents jeux et menu

--- GeoQuizz/js/flagGuesser/flagGuesser.js
+++ GeoQuizz/js/flagGuesser/flagGuesser.js
@@ -12,28 +12,28 @@ let questionNumber = 1;
 let questionTotal = 3;
 let goodAnswers = 0;
 
-function createButton($class,$text) {  
-    var myDiv = document.getElementById("answer");  
+function createButton($class, $text) {
+    var myDiv = document.getElementById("answer");
     // On créer le bouton  
-    var button = document.createElement('BUTTON');  
+    var button = document.createElement('BUTTON');
     // Texte du bouton
-    var text = document.createTextNode($text); 
+    var text = document.createTextNode($text);
     //Type du bouton
     button.type = 'button'
     //classe du bouton
     button.className += $class;
-    // appending text to button 
-    button.appendChild(text); 
+    // appending text to button
+    button.appendChild(text);
     // appending button to div 
     myDiv.appendChild(button);
 
     button.addEventListener('click', () => {
-        if(questionNumber <= questionTotal){
+        if (questionNumber <= questionTotal) {
             genererateQuestion(); //On recrée une question
             switchState('question'); //on passe à la question suivante
-        }else{ //Si il n'y en a plus alors on affiche le score dans le end state
-            state.end.querySelector('p').innerHTML = `Votre score est: ${goodAnswers} / ${questionTotal}`
-            state.end.querySelector('a').innerHTML += '<button class="btn btn-primary"> Retour Menu </button>'
+        } else { //Si il n'y en a plus alors on affiche le score dans le end state
+            document.getElementById('end').innerHTML += `<p> Votre score est: ${goodAnswers} / ${questionTotal} </p>`
+            document.getElementById('end').innerHTML += '<p id="pfin"> <a class ="button2" href="menu.html">  Retour Menu </a> </p>'
             switchState('end');
         }
     });
@@ -57,10 +57,10 @@ const init = async () => {
 
 // Cliquer sur une des reponses
 const handleClickChoice = () => {
-    state.question.querySelector('ul').addEventListener('click',({target}) => {
-        if (target.matches('li')){
-             const userAnswer = target.innerHTML;
-             checkAnswer(userAnswer);
+    state.question.querySelector('ul').addEventListener('click', ({ target }) => {
+        if (target.matches('li')) {
+            const userAnswer = target.innerHTML;
+            checkAnswer(userAnswer);
         }
     });
 }
@@ -73,14 +73,14 @@ const genererateQuestion = () => {
 
     //Aficher suivi des questions
     state.question.querySelector("p").innerHTML = " Question " + questionNumber + " / " + questionTotal;
- 
+
 
     // Afficher un drapeau
-    state.question.querySelector("img").setAttribute("src",questions.flag);
+    state.question.querySelector("img").setAttribute("src", questions.flag);
 
     //Ajouter les choix de reponses sur la page
     const reponses = questions.possibilities.map((possibility) => {
-        return `<li id="response" class="btn btn-primary">${possibility}</li>`;
+        return `<li id="response" class="btnanswer police">${possibility}</li>`;
     });
     state.question.querySelector("ul").innerHTML = reponses.join('');
 }
@@ -97,28 +97,29 @@ const createQuestion = (countries) => {
 
     // Liste des reponses
     const possibilities = [];
-    
-    while(possibilities.length < 3) { //On affiche 3 reponses random
+
+    while (possibilities.length < 3) { //On affiche 3 reponses random
         const r = parseInt(Math.random() * countries.length);
         const name = countries[r].translations.fr;
-        if(!possibilities.includes(name) && name != country.translations.fr) {
+        if (!possibilities.includes(name) && name != country.translations.fr) {
             possibilities.push(name);
         }
-        else{
+        else {
             console.log("doublon")
         }
-        
+
     }
+    
 
     possibilities.push(country.translations.fr); //on ajoute la bonne reponse
-    possibilities.sort((a,b) =>{ //on tri la liste des reponses
+    possibilities.sort((a, b) => { //on tri la liste des reponses
         return a.charCodeAt(0) - b.charCodeAt(0);
     });
-    
+
 
     // Tout ce qui va nous servir à creer la question (un drapeau, un choix de reponse, une bonne reponse)
     const questions = {
-        flag: country.flag,  
+        flag: country.flag,
         answer: country.translations.fr,
         possibilities
     }
@@ -127,27 +128,27 @@ const createQuestion = (countries) => {
 }
 
 //Passer d'un état à l'autre (question, verification de reponse)
-const switchState = (states) =>{
-    switch(states){
+const switchState = (states) => {
+    switch (states) {
         case 'answer':
             state.answer.style.display = 'block';
             state.question.style.display = 'none';
-            state.end.style.display ='none';
-            if  (state.answer.contains(document.querySelector('button') ) )  {
+            state.end.style.display = 'none';
+            if (state.answer.contains(document.querySelector('button'))) {
             }
-            else{
-                createButton('btn btn-primary','Question suivante')
+            else {
+                createButton('btn btn-primary', 'Question suivante')
             }
             break;
         case 'question':
             state.answer.style.display = 'none';
             state.question.style.display = 'block';
-            state.end.style.display ='none';
+            state.end.style.display = 'none';
             break;
         default:
             state.answer.style.display = 'none';
             state.question.style.display = 'none';
-            state.end.style.display ='block';
+            state.end.style.display = 'block';
             break;
     }
 };
@@ -156,17 +157,19 @@ const switchState = (states) =>{
 // Verifier si c'est la bonne reponse
 const checkAnswer = (userAnswer) => {
     // si oui alors bonne reponse
-    if(userAnswer === questions.answer){
-        state.answer.querySelector('h2').innerHTML ='Bonne réponse';
-        state.answer.querySelector('p').innerHTML ='';
+    if (userAnswer === questions.answer) {
+        state.answer.querySelector('h2').style.color = 'green'
+        state.answer.querySelector('h2').innerHTML = 'Bonne réponse !';
+        state.answer.querySelector('p').innerHTML = '';
         goodAnswers++;
-    }else{
+    } else {
         // si non alors mauvais reponse
-        state.answer.querySelector('h2').innerHTML ='Mauvaise réponse';
-        state.answer.querySelector('p').innerHTML =`La réponse était : ${questions.answer}`; 
+        state.answer.querySelector('h2').style.color = 'red'
+        state.answer.querySelector('h2').innerHTML = 'Mauvaise réponse !';
+        state.answer.querySelector('p').innerHTML = `La réponse était : ${questions.answer}`;
 
     }
-    questionNumber++; 
+    questionNumber++;
     //afficher la reponse dans state answer
     switchState('answer');
 }
