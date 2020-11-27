8369c7130f5d7750b255004c81ac65fb544ac191
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2020-11-27 18:05:07 +0100

    #13 finalisation du jeu de test flagGuesser et installation de selenium

--- GeoQuizz/test/flagGuesserSpec.js
+++ GeoQuizz/test/flagGuesserSpec.js
@@ -1,30 +1,67 @@
-const jsdom = require('mocha-jsdom');
-const expect = require('chai').expect
+const { Builder, By, Key, util } = require('selenium-webdriver');
+let webdriver = require('selenium-webdriver'), until = webdriver.until;
+async function testFlagGuesser() {
+    let driver = await new Builder().forBrowser('chrome').build();
 
-describe('Test de flagGuesser', function () {
+    await driver.get("C:/Users/KHALI/Desktop/marais/projet-web-2020-marais/GeoQuizz/menu.html");
+    driver.findElement(By.className("button2")).click();
 
-    beforeEach(() => {
-        jsdom();
-    })
+    console.log("Statut de l'affichage de la question " + await driver.wait(until.elementLocated(By.id('question')), 10000).isDisplayed());
 
-    it('should generate a question', function () {
+    console.log("Statut de l'affichage du nombres de question " + await driver.wait(until.elementLocated(By.id("nbquestion")), 10000).isDisplayed());
 
-    })
+    console.log("Statut de l'affichage du drapeau " + await driver.wait(until.elementLocated(By.id("jumboflag")), 10000).isDisplayed());
 
-    it('should create a question', function () {
+    console.log("Statut de l'affichage d'une reponse " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
 
-    })
+    driver.findElement(By.id("response")).click();
 
-    it('should change the state of the game', function () {
+    console.log("Statut de l'affichage d'un titre " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
 
-    })
 
-    it('should get the answer of the player', function () {
+    console.log("Statut de l'affichage d'un bouton 'question suivante' " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
 
-    })
+    driver.findElement(By.className("btn btn-primary")).click();
 
-    it('should check the answer of the player', function () {
+    console.log("Statut de l'affichage de la question de la page 2  " + await driver.wait(until.elementLocated(By.id('question')), 10000).isDisplayed());
 
-    })
+    console.log("Statut de l'affichage du nombres de question de la page 2 " + await driver.wait(until.elementLocated(By.id("nbquestion")), 10000).isDisplayed());
 
-})
+    console.log("Statut de l'affichage du drapeau de la page 2 " + await driver.wait(until.elementLocated(By.id("jumboflag")), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage d'une reponse de la page 2 " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
+
+    driver.findElement(By.id("response")).click();
+
+    console.log("Statut de l'affichage d'un titre de la page 2 " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
+
+
+    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 2 " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
+
+    driver.findElement(By.className("btn btn-primary")).click();
+
+    console.log("Statut de l'affichage de la question de la page 3  " + await driver.wait(until.elementLocated(By.id('question')), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage du nombres de question de la page 3 " + await driver.wait(until.elementLocated(By.id("nbquestion")), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage du drapeau de la page 3 " + await driver.wait(until.elementLocated(By.id("jumboflag")), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage d'une reponse de la page 3 " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
+
+    driver.findElement(By.id("response")).click();
+
+    console.log("Statut de l'affichage d'un titre de la page 3 " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 3 " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
+
+    driver.findElement(By.className("btn btn-primary")).click();
+
+    console.log("Statut de l'affichage de l'affichage du score " + await driver.wait(until.elementLocated(By.id('end')), 10000).isDisplayed());
+
+    console.log("Statut de l'affichage d'un bouton 'retour menu'  " + await driver.wait(until.elementLocated(By.className("button2")), 10000).isDisplayed());
+
+    driver.findElement(By.className("button2")).click();
+    driver.quit();
+
+}
+testFlagGuesser();
