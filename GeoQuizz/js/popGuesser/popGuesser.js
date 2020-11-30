1e900a0686ac50a40b543b55133952f40433d03e
Corentin <corentingarnier06@gmail.com>
2020-11-30 18:28:08 +0100

    #43 #42 PopGuesser avancement

--- GeoQuizz/js/popGuesser/popGuesser.js
+++ GeoQuizz/js/popGuesser/popGuesser.js
@@ -1,10 +1,43 @@
 let state = {
     question: null,
+    answer: null,
+    end: null,
+
 }
 
 let countries = [];
+let questions = {};
 let questionNumber = 1;
 let questionTotal = 3;
+let score = 0;
+
+function createButton($class, $text) {
+    var myDiv = document.getElementById("answer");
+    // On créer le bouton  
+    var button = document.createElement('BUTTON');
+    // Texte du bouton
+    var text = document.createTextNode($text);
+    //Type du bouton
+    button.type = 'button'
+    //classe du bouton
+    button.className += $class;
+    // appending text to button 
+    button.appendChild(text);
+    // appending button to div 
+    myDiv.appendChild(button);
+
+    button.addEventListener('click', () => {
+        if (questionNumber <= questionTotal) {
+            genererateQuestion(); //On recrée une question
+            switchState('question'); //on passe à la question suivante
+
+        } else { //Si il n'y en a plus alors on affiche le score dans le end state
+            state.end.querySelector('p').innerHTML = 'Votre score est:' + score + '/' + questionTotal;
+            state.end.querySelector('a').innerHTML += '<button class="btn btn-primary"> Retour Menu </button>'
+            switchState('end');
+        }
+    });
+}
 
 const init = async () => {
     state.question = document.querySelector("#question");
@@ -91,4 +124,29 @@ const createQuestion = (countries) => {
     console.log(questions.possibilities[1])
 
     return questions;
+}
+
+const switchState = (states) => {
+    switch (states) {
+        case 'answer':
+            state.answer.style.display = 'block';
+            state.question.style.display = 'none';
+            state.end.style.display = 'none';
+            if (state.answer.contains(document.querySelector('button'))) {
+            }
+            else {
+                createButton('btn btn-primary', 'Question suivante')
+            }
+            break;
+        case 'question':
+            state.answer.style.display = 'none';
+            state.question.style.display = 'block';
+            state.end.style.display = 'none';
+            break;
+        default:
+            state.answer.style.display = 'none';
+            state.question.style.display = 'none';
+            state.end.style.display = 'block';
+            break;
+    }
 }
