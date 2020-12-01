543172044ba8c278c34348990c0cd589bf57aebc
Guilhem Fabre <guilhem.fabre@cegetel.net>
2020-12-01 21:10:43 +0100

    #50 Affichage des réponses de CapitalCity

--- GeoQuizz/js/CapitalCity/capitalCity.js
+++ GeoQuizz/js/CapitalCity/capitalCity.js
@@ -2,7 +2,7 @@ let state = {
     question: null,
 }
 
-let countries = [];
+let countries;
 let questionNumber = 1;
 let questionTotal = 3;
 
@@ -10,8 +10,15 @@ const init = async () => {
     state.question = document.querySelector("#question");
 
     // Acces à toutes les informations des pays
+
+
     const response = await fetch("https://restcountries.eu/rest/v2/all");
     countries = await response.json();
+    
+    //Methode pour quand on aura le serveur
+    //const geo = require('geo');
+    //et rawdata = geo.readFileSync('geo.json');
+    //countries = JSON.parse(rawdata);
 
     generateQuestion();
 }
@@ -29,6 +36,13 @@ const generateQuestion = () => {
     // Afficher la question
     state.question.querySelector("#questionTitle").innerHTML = "Quel est la capitale de ce pays ?";
     state.question.querySelector("#pays").innerHTML = questions.pays;
+
+    //Ajouter les choix de reponses sur la page
+    const reponses = questions.possibilities.map((possibility) => {
+        return `<li id="response" class="btnanswer police">${possibility}</li>`;
+    });
+    state.question.querySelector("ul").innerHTML = reponses.join('');
+
 }
 
 
@@ -37,10 +51,38 @@ const createQuestion = (countries) => {
 
 
     // Pays aleatoire parmis la liste
+    while (countries[random].capital == "") {
+        random = parseInt(Math.random() * countries.length);
+    }
     const country = countries[random];
 
+    // Liste des reponses
+    const possibilities = [];
+
+    while (possibilities.length < 3) { //On affiche 3 reponses random
+        const r = parseInt(Math.random() * countries.length);
+        while (countries[r].capital == "") {
+            r = parseInt(Math.random() * countries.length);
+        }
+        const capCity = countries[r].capital;
+        if (!possibilities.includes(capCity) && capCity != country.capital) {
+            possibilities.push(capCity);
+        }
+        else {
+            console.log("doublon")
+        }
+
+    }
+
+    possibilities.push(country.capital); //on ajoute la bonne reponse
+    possibilities.sort((a, b) => { //on tri la liste des reponses
+        return a.charCodeAt(0) - b.charCodeAt(0);
+    });
+
     const questions = {
-        pays: country.translations.fr
+        cap: country.capital,
+        pays: country.translations.fr,
+        possibilities
     }
 
 
