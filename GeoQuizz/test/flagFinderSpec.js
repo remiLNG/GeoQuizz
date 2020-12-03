12a061d1482e2a392ed9303f18c5f3631b9807c8
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2020-12-03 16:22:37 +0100

    #21 Modification de la classe flagFinderSpec

--- GeoQuizz/test/flagFinderSpec.js
+++ GeoQuizz/test/flagFinderSpec.js
@@ -1,30 +1,17 @@
-const jsdom = require('mocha-jsdom');
-const expect = require('chai').expect
+const { Builder, By, Key, util } = require('selenium-webdriver');
+let webdriver = require('selenium-webdriver'), until = webdriver.until;
+async function testFlagFinder() {
+    let driver = await new Builder().forBrowser('chrome').build();
 
-describe('Test de flagFinder', function () {
+    await driver.get("C:/Users/KHALI/Desktop/marais/projet-web-2020-marais/GeoQuizz/FlagFinder.html");
+    driver.manage().window().maximize();
 
-    beforeEach(() => {
-        jsdom();
-    })
-
-    it('should generate a question', function () {
-
-    })
-
-    it('should create a question', function () {
-
-    })
-
-    it('should change the state of the game', function () {
-
-    })
-
-    it('should get the answer of the player', function () {
-
-    })
-
-    it('should check the answer of the player', function () {
-
-    })
-
-})
+    /*List <WebElement> listButton;
+    listButton=driver.findElements(By.className("button2")) ;
+    listButton.get[1].click();*/
+    console.log("Statut du suivie des questions " + await driver.wait(until.elementLocated(By.id('suiviQuestion')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la questions " + await driver.wait(until.elementLocated(By.id('questionTitle')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du pays " + await driver.wait(until.elementLocated(By.id('pays')), 10000).isDisplayed());
+    console.log("Statut de l'affichage des drapeau  " + await driver.wait(until.elementLocated(By.className('jumbotron')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.id('flag1')), 10000).click();
+} testFlagFinder();
