c76bb8ecfbc9535dd53fca3b4003cbe1d38faf62
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2020-12-04 17:06:59 +0100

    #21 finalisation de la classe de test flagFinderSpec

--- GeoQuizz/test/flagFinderSpec.js
+++ GeoQuizz/test/flagFinderSpec.js
@@ -3,15 +3,44 @@ let webdriver = require('selenium-webdriver'), until = webdriver.until;
 async function testFlagFinder() {
     let driver = await new Builder().forBrowser('chrome').build();
 
-    await driver.get("C:/Users/KHALI/Desktop/marais/projet-web-2020-marais/GeoQuizz/FlagFinder.html");
+    await driver.get("C:/Users/KHALI/Desktop/marais/projet-web-2020-marais/GeoQuizz/menu.html");
     driver.manage().window().maximize();
 
-    /*List <WebElement> listButton;
-    listButton=driver.findElements(By.className("button2")) ;
-    listButton.get[1].click();*/
+    driver.findElement(By.xpath("//a[@href='./FlagFinder.html']")).click();
     console.log("Statut du suivie des questions " + await driver.wait(until.elementLocated(By.id('suiviQuestion')), 10000).isDisplayed());
     console.log("Statut de l'affichage de la questions " + await driver.wait(until.elementLocated(By.id('questionTitle')), 10000).isDisplayed());
     console.log("Statut de l'affichage du pays " + await driver.wait(until.elementLocated(By.id('pays')), 10000).isDisplayed());
     console.log("Statut de l'affichage des drapeau  " + await driver.wait(until.elementLocated(By.className('jumbotron')), 10000).isDisplayed());
     driver.wait(until.elementLocated(By.id('flag1')), 10000).click();
+    console.log("Statut de l'affichage du titre  " + await driver.wait(until.elementLocated(By.tagName('h2')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la reponse si elle est fausse  " + await driver.wait(until.elementLocated(By.id('mauvaiserep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la bonne reponse  " + await driver.wait(until.elementLocated(By.id('rep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la  reponse  " + await driver.wait(until.elementLocated(By.id('bonrep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du bon drapeau  " + await driver.wait(until.elementLocated(By.id('goodflag')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.className('btn btn-primary')), 10000).click();
+    console.log("Statut du suivie des questions '2' " + await driver.wait(until.elementLocated(By.id('suiviQuestion')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la questions '2' " + await driver.wait(until.elementLocated(By.id('questionTitle')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du pays '2'" + await driver.wait(until.elementLocated(By.id('pays')), 10000).isDisplayed());
+    console.log("Statut de l'affichage des drapeau '2' " + await driver.wait(until.elementLocated(By.className('jumbotron')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.id('flag1')), 10000).click();
+    console.log("Statut de l'affichage du titre '2' " + await driver.wait(until.elementLocated(By.tagName('h2')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la reponse si elle est fausse '2' " + await driver.wait(until.elementLocated(By.id('mauvaiserep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la bonne reponse '2' " + await driver.wait(until.elementLocated(By.id('rep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la  reponse '2' " + await driver.wait(until.elementLocated(By.id('bonrep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du bon drapeau  '2'" + await driver.wait(until.elementLocated(By.id('goodflag')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.className('btn btn-primary')), 10000).click();
+    console.log("Statut du suivie des questions '3'" + await driver.wait(until.elementLocated(By.id('suiviQuestion')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la questions '3'" + await driver.wait(until.elementLocated(By.id('questionTitle')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du pays '3'" + await driver.wait(until.elementLocated(By.id('pays')), 10000).isDisplayed());
+    console.log("Statut de l'affichage des drapeau  '3'" + await driver.wait(until.elementLocated(By.className('jumbotron')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.id('flag1')), 10000).click();
+    console.log("Statut de l'affichage du titre '3' " + await driver.wait(until.elementLocated(By.tagName('h2')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la reponse si elle est fausse '3' " + await driver.wait(until.elementLocated(By.id('mauvaiserep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la bonne reponse '3' " + await driver.wait(until.elementLocated(By.id('rep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage de la  reponse '3' " + await driver.wait(until.elementLocated(By.id('bonrep')), 10000).isDisplayed());
+    console.log("Statut de l'affichage du bon drapeau  '3'" + await driver.wait(until.elementLocated(By.id('goodflag')), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.className('btn btn-primary')), 10000).click();
+    console.log("Statut de l'affichage de l'affichage du score " + await driver.wait(until.elementLocated(By.id('end')), 10000).isDisplayed());
+    console.log("Statut de l'affichage d'un bouton 'retour menu'  " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
+    driver.wait(until.elementLocated(By.className('btn btn-primary')), 10000).click();
 } testFlagFinder();
