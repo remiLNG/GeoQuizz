974534b3dc2ac4d8bfd9dd995088c4d24d34bfe9
Corentin <corentingarnier06@gmail.com>
2020-12-17 03:05:00 +0100

    #65

--- GeoQuizz/index.js
+++ GeoQuizz/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const flagfinder = require('./routers/FlagFinder');
 const flagguesser = require('./routers/flagguesser');
@@ -11,15 +12,15 @@ const multiplayer = require('./routers/multiplayer');
 
 const PORT = 8085;
 
-app.use('/css',express.static( __dirname +'/css'));
-app.use('/assets',express.static( __dirname +'/assets'));
-app.use('/js',express.static( __dirname +'/js'));
-app.use('/fonts',express.static( __dirname +'/fonts'));
-app.use('/sounds',express.static( __dirname +'/sounds'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/sounds', express.static(__dirname + '/sounds'));
 
 
-app.get('/menu', (req,res) => {
-    res.sendFile(path.join( __dirname, "menu.html"))
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, "menu.html"))
 })
 
 app.use('/flagfinder', flagfinder);
@@ -32,10 +33,18 @@ app.use('/CapitalCity', capitalcity);
 
 app.use('/Multiplayer', multiplayer);
 
-app.get('/Regle', (req,res) => {
-    res.sendFile(path.join( __dirname, "Regle.html"))
+app.get('/Regle', (req, res) => {
+    res.sendFile(path.join(__dirname, "Regle.html"))
 })
+
+io.on('connect', (client) => {
+
+    client.on('JoinGame', () => {
+        console.log('A player want to join a game')
+    });
+
+});
 
 http.listen(PORT, () => {
     console.log(`Server currently running on port ${PORT}`)
-  })
+})
