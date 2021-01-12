19c992bb265422478af56f482917e290718928b9
Hassen Khalifa <khalifa.hassen.pro@gmail.com>
2021-01-12 17:18:09 +0100

    #95 correction du test flagGuesserSpec

--- GeoQuizz/test/flagGuesserSpec.js
+++ GeoQuizz/test/flagGuesserSpec.js
@@ -10,6 +10,7 @@ async function testFlagGuesser() {
     'edge' pour Microsoft edge
     */
     let driver = await new Builder().forBrowser('chrome').build();
+    driver.manage().window().maximize();
 
     await driver.get("C:/Users/KHALI/Desktop/marais/projet-web-2020-marais/GeoQuizz/FlagGuesser.html");
     //driver.findElement(By.className("button2")).click();
@@ -19,24 +20,24 @@ async function testFlagGuesser() {
     console.log("Statut de l'affichage d'une reponse " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
     driver.findElement(By.id("response")).click();
     console.log("Statut de l'affichage d'un titre " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
-    console.log("Statut de l'affichage d'un bouton 'question suivante' " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
-    driver.findElement(By.className("btn btn-primary")).click();
+    console.log("Statut de l'affichage d'un bouton 'question suivante' " + await driver.wait(until.elementLocated(By.className("button2")), 10000).isDisplayed());
+    driver.findElement(By.id("retourmenu")).click();
     console.log("Statut de l'affichage de la question de la page 2  " + await driver.wait(until.elementLocated(By.id('question')), 10000).isDisplayed());
     console.log("Statut de l'affichage du nombres de question de la page 2 " + await driver.wait(until.elementLocated(By.id("nbquestion")), 10000).isDisplayed());
     console.log("Statut de l'affichage du drapeau de la page 2 " + await driver.wait(until.elementLocated(By.id("jumboflag")), 10000).isDisplayed());
     console.log("Statut de l'affichage d'une reponse de la page 2 " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
     driver.findElement(By.id("response")).click();
     console.log("Statut de l'affichage d'un titre de la page 2 " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
-    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 2 " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
-    driver.findElement(By.className("btn btn-primary")).click();
+    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 2 " + await driver.wait(until.elementLocated(By.className("button2")), 10000).isDisplayed());
+    driver.findElement(By.id("retourmenu")).click();
     console.log("Statut de l'affichage de la question de la page 3  " + await driver.wait(until.elementLocated(By.id('question')), 10000).isDisplayed());
     console.log("Statut de l'affichage du nombres de question de la page 3 " + await driver.wait(until.elementLocated(By.id("nbquestion")), 10000).isDisplayed());
     console.log("Statut de l'affichage du drapeau de la page 3 " + await driver.wait(until.elementLocated(By.id("jumboflag")), 10000).isDisplayed());
     console.log("Statut de l'affichage d'une reponse de la page 3 " + await driver.wait(until.elementLocated(By.id("response")), 10000).isDisplayed());
-    driver.findElement(By.id("response")).click();
+    driver.findElement(By.className("button2")).click();
     console.log("Statut de l'affichage d'un titre de la page 3 " + await driver.wait(until.elementLocated(By.tagName("h2")), 10000).isDisplayed());
-    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 3 " + await driver.wait(until.elementLocated(By.className("btn btn-primary")), 10000).isDisplayed());
-    driver.findElement(By.className("btn btn-primary")).click();
+    console.log("Statut de l'affichage d'un bouton 'question suivante' de la page 3 " + await driver.wait(until.elementLocated(By.className("button2")), 10000).isDisplayed());
+    driver.findElement(By.id("retourmenu")).click();
     console.log("Statut de l'affichage de l'affichage du score " + await driver.wait(until.elementLocated(By.id('end')), 10000).isDisplayed());
     console.log("Statut de l'affichage d'un bouton 'retour menu'  " + await driver.wait(until.elementLocated(By.className("button2")), 10000).isDisplayed());
     driver.findElement(By.className("button2")).click();
